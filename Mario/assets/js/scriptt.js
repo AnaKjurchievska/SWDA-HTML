8aa3af34d56c05caa86c35ceec9c3c88a99eddf4
AnaKjurchievska <43668869+AnaKjurchievska@users.noreply.github.com>
2018-12-24 12:23:22 +0100

    mario

---
+++ Mario/assets/js/scriptt.js
@@ -0,0 +1,79 @@
+
+
+// console.log("test");
+// console.log(523);
+// console.log(5+5)
+
+
+
+// var name="MArio";
+// console.log(name);
+
+
+// alert(name);
+
+
+
+// function helloWorld(){
+//     return "Hello World";
+
+// }
+
+// console.log(helloworld())
+
+
+// Function otherHelloWorld(){
+// console.log("HEloo World");
+// }
+// otherHelloWorld();
+
+
+// function helloName(name) {
+// console.log("Heloo" + name);
+
+// }
+
+// helloName("MArio")
+
+
+// function dogYears(age) {
+//     console.log(" MY dog is " + age + " and that makes him "  +  (age * 7) + 
+//         " years.");
+// }
+
+// dogYears(5);
+
+// function fortuneTeller(name,ocupation,city,age) {
+// return "My name is" + name + "I am " + ocupation + "i live in" + city + "i have" + age + "old";
+
+// }
+
+// console.log(fortuneTeller("Ana", "Front End Developer", "Skopje", 25));
+
+
+// function rollerCoasterRider(height, limit) {
+// if(height < limit){
+//     return "You can ride the rollercoaster";
+// } else{
+//     return "You cant ride the rollercoaster";
+// }
+
+
+// }
+
+// console.log( rollerCoasterRider (1.83, 1.68));
+
+
+
+// function numberComparison(a,b) {
+// if(a > b) {
+// return "The number" + a + "is grether then" + b;
+// } else if(a < b) {
+//     return "Number" + b + "is greater then" + a;
+// }else {
+//     return "Numbers are equal.";
+// }
+
+
+
+// console.log(numberComparison (5,10));
